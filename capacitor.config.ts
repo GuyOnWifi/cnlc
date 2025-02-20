b53de3a740fa04e79e36039762c54ad2aa65286b
Eason Huang <easonh887@gmail.com>
2025-02-19 20:49:33 -0500

    I tried adding capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.guyonwifi.cnlc',
+  appName: 'cnlc',
+  webDir: 'out'
+};
+
+export default config;
